3bc974e91be1433b3a8c29c369e9f31e0f37058f
Daniel McKinney <adrmckinney@gmail.com>
2021-03-19 11:19:17 -0400

    first commit

---
+++ core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+PNOPTIONS = (
+    ("she/her/hers", "she/her/hers"),
+    ("he/him/his", "he/him/his"),
+    ("they/them/theirs", "they/them/theirs")
+)
+
+class User(AbstractUser):
+    name = models.CharField(max_length=225, blank=True, null=True, default="")
+
+class UserProfile(models.Model):
+    name = models.CharField(max_length=225)
+    pronouns = models.CharField(max_length=100, choices=PNOPTIONS, null=True)
+    email = models.CharField(max_length=225, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
