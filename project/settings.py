c1cc833cc64a8f5f84657978d8b3d3bd7965d7a8
Daniel McKinney <adrmckinney@gmail.com>
2021-03-19 20:24:51 -0400

    bugs

--- project/settings.py
+++ project/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
